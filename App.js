8c0d7d3d69d5740fa6df20b01eca85ef2c8d0811
pedroarthur22 <167103123+pedroarthur22@users.noreply.github.com>
2026-09-24 09:02:35 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Image, Pressable } from "react-native";
 
-export default function App() {
+const fotos = [
+  "https://picsum.photos/id/1036/300/300",
+  "https://picsum.photos/seed/foto2/300/300",
+  "https://picsum.photos/seed/foto3/300/300",
+  "https://picsum.photos/seed/foto4/300/300",
+  "https://picsum.photos/seed/foto5/300/300",
+  "https://picsum.photos/seed/foto6/300/300"
+];
+
+
+function Foto({ imagem }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Image
+      source={{ uri: imagem }}
+      style={{
+        width: 300,
+        height: 300
+      }}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+
+  const [foto, setFoto] = useState(0);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+
+      
+      <Text
+        style={{
+          fontSize: 25,
+          marginBottom: 20
+        }}
+      >
+        Meu Álbum de Fotos
+      </Text>
+
+
+      
+      <Foto imagem={fotos[foto]} />
+
+
+      
+      <Text
+        style={{
+          marginTop: 10
+        }}
+      >
+        Foto {foto + 1}
+      </Text>
+
+
+      
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 20
+        }}
+      >
+
+        <Pressable
+          onPress={() => {
+            if (foto > 0) {
+              setFoto(foto - 1);
+            }
+          }}
+        >
+          <Text
+            style={{
+              marginRight: 60
+            }}
+          >
+            ANTERIOR
+          </Text>
+        </Pressable>
+
+
+        <Pressable
+          onPress={() => {
+            if (foto < 5) {
+              setFoto(foto + 1);
+            }
+          }}
+        >
+          <Text>
+            PRÓXIMA
+          </Text>
+        </Pressable>
+
+      </View>
+
+    </View>
+  );
+}
